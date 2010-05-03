939aca3718eaf8bc04ecf2883ac9358ac0967406
Ryan Brainard <rbrainard@salesforce.com>
2010-05-03 02:48:48 +0000

    Fix DemoAuthenicator to require username and password not be null. (eclipse)

--- src/main/java/axiom/delauth/token/DemoAuthenticator.java
+++ src/main/java/axiom/delauth/token/DemoAuthenticator.java
@@ -3,7 +3,7 @@ package axiom.delauth.token;
 public class DemoAuthenticator implements Authenticator {
 
 	public boolean authenticate(String username, String password) {
-		return password.equals("123456");
-	}
+        return !(null == username || null == password) && password.equals("123456");
+    }
 
 }
