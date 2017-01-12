b2a836fbb7169b6849d7b9b21632f9e1bcf40afe
Ryan Guest <(none)m>
2017-01-12 15:29:53 -0800

    Fix typo

--- src/main/java/com/sforce/soap/authentication/AuthenticationPortType.java
+++ src/main/java/com/sforce/soap/authentication/AuthenticationPortType.java
@@ -4,7 +4,7 @@ package com.sforce.soap.authentication;
 public interface AuthenticationPortType extends java.rmi.Remote {
 
     /**
-     * Are the supplied saleforce.com userId and password valid ?
+     * Are the supplied salesforce.com userId and password valid ?
      */
     public com.sforce.soap.authentication.AuthenticateResult authenticate(com.sforce.soap.authentication.Authenticate parameters) throws java.rmi.RemoteException;
 }
